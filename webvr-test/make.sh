0efee3e97b492f15033dfbf632151f9d4023a05a
Bhautik Joshi <bjoshi+github@gmail.com>
2015-10-29 22:54:27 -0700

    oops - don't need to recompress three every time

--- webvr-test/make.sh
+++ webvr-test/make.sh
@@ -3,7 +3,7 @@
 cat src/js-ext/webvr-polyfill.js | uglifyjs -cm > src/js-ext/webvr-polyfill.min.js
 cat src/js-ext/webvr-manager.js | uglifyjs -cm > src/js-ext/webvr-manager.min.js
 cat src/js-ext/VRControls.js | uglifyjs -cm > src/js-ext/VRControls.min.js
-cat src/js-ext/three.js | uglifyjs -cm > bin/three.min.js
+# cat src/js-ext/three.js | uglifyjs -cm > bin/three.min.js
 
 
 browserify   src/vr-bundle-main.js > bin/vr-bundle.js
