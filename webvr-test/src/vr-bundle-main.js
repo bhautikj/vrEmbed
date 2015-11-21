825dca8adec177354fe6387a28ea2676b1bbb53d
Bhautik Joshi <bjoshi+github@gmail.com>
2015-11-21 14:15:36 -0800

    even with updates, event handling is rubbish
    gotta throw out webvr-* for now :(

--- webvr-test/src/vr-bundle-main.js
+++ webvr-test/src/vr-bundle-main.js
@@ -15,12 +15,6 @@ require('./js-ext/VRControls.min.js');
 // //enable HMD mode without being mobile device
 // //forces WebVRPolyfill.prototype.isCardboardCompatible to return true
 window.CARDBOARD_DEBUG = true;
-// 
-// //   A polyfill for WebVR using the Device{Motion,Orientation}Event API.
-require('./js-ext/webvr-polyfill.min.js');
-// 
-// //   Helps enter and exit VR mode, provides best practices while in VR.
-require('./js-ext/webvr-manager.min.js');
 
 // 
 // 
@@ -37,6 +31,25 @@ require('./js-ext/webvr-manager.min.js');
 WEBVR_PREDICTION_MODE = 0;
 // In prediction mode, change how far into the future to predict.
 //WEBVR_PREDICTION_TIME_MS = 100;// </script>
+WebVRConfig = {
+  // Forces availability of VR mode.
+  //FORCE_ENABLE_VR: true, // Default: false.
+  // Complementary filter coefficient. 0 for accelerometer, 1 for gyro.
+  //K_FILTER: 0.98, // Default: 0.98.
+  // How far into the future to predict during fast motion.
+  //PREDICTION_TIME_S: 0.050, // Default: 0.050s.
+  // Flag to disable touch panner. In case you have your own touch controls
+  TOUCH_PANNER_DISABLED: true, // Default: false.
+  // To disable keyboard and mouse controls. In case you implement your own
+  MOUSE_KEYBOARD_CONTROLS_DISABLED: true // Default: false
+}
+
+// 
+// //   A polyfill for WebVR using the Device{Motion,Orientation}Event API.
+require('./js-ext/webvr-polyfill.min.js');
+// 
+// //   Helps enter and exit VR mode, provides best practices while in VR.
+require('./js-ext/webvr-manager.min.js');
 
 require('./js/VRParser.js');
 require('./js/VRIcons.js');
