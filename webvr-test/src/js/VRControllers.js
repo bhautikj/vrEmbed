276539a2cfec45e26fc102be54f93e2005bcc07e
Bhautik Joshi <bjoshi+github@gmail.com>
2015-11-30 19:21:21 -0800

    camera spinner hookup

--- webvr-test/src/js/VRControllers.js
+++ webvr-test/src/js/VRControllers.js
@@ -1,33 +1,62 @@
 function VRLookControlBase() {
-  this.callbacks = {};
+  var self = this;
+  this.eulerX = 0.0;
+  this.eulerY = 0.0;
+  this.eulerZ = 0.0;
+  this.camera = null;
 }
 
-VRLookControlBase.prototype.update = function() {
+VRLookControlBase.prototype.updateBase = function() {
+    if (this.camera == null){
+      alert("OH GODS");
+      return;
+    }
+//     alert(this.eulerY);
+    var devm = new THREE.Quaternion().setFromEuler(
+          new THREE.Euler(this.eulerX, this.eulerY, this.eulerZ));
+    this.camera.quaternion.copy( devm );
 };
 
-// TODO:
-// set camera
-// dispose
-// update
+VRLookControlBase.prototype.setEuler = function(x,y,z) {
+  this.eulerX = x;
+  this.eulerY = y;
+  this.eulerZ = z;
+};
+
+VRLookControlBase.prototype.setCamera = function(camera) {
+  this.camera = camera;
+};
+
+VRLookControlBase.prototype.dispose = function() {
+  this.camera = null;
+};
+
+
+VRIdleCameraSpinner = function() {
+}
+
+VRIdleCameraSpinner.prototype = new VRLookControlBase();
+
+VRIdleCameraSpinner.prototype.update = function(){
+  this.setEuler(0, this.eulerY+0.01, 0);
+//   console.log(this.eulerY);
+  this.updateBase();
+}
 
 VRLookController = function() {
   var self = this;
-  this.camera = null;
   this.angle = 0;
-  
+  this.idleCameraSpinner = new VRIdleCameraSpinner();
   
   this.setCamera = function(camera){
-    self.camera = camera;
+    self.idleCameraSpinner.setCamera(camera);
   };
   
   this.update = function() {
-    if (self.camera==null)
+    if (self.idleCameraSpinner==null)
       return;
-    
-    self.angle += 0.01;
-    var devm = new THREE.Quaternion().setFromEuler(
-          new THREE.Euler(0, self.angle, self.angle));
-    self.camera.quaternion.copy( devm );
+
+    self.idleCameraSpinner.update();
   };
   
 };
