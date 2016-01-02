e99cc5ab8dcb7fdc8af61e7fe0289f460b9358cb
Bhautik Joshi <bjoshi+github@gmail.com>
2016-01-02 00:31:53 -0800

    hooked up resize events

--- src/js/VRtwglQuad.js
+++ src/js/VRtwglQuad.js
@@ -1,5 +1,3 @@
-"use strict"
-
 var twgl = require('../js-ext/twgl.js');
 
 VRtwglQuad = function() {
@@ -7,12 +5,15 @@ VRtwglQuad = function() {
   this.glContext = null;
   this.programInfo = null;
   this.bufferInfo = null;
+  this.parentElement = null;
+  var self = this;
 
   this.init = function (elm, vs, fs){
+    this.parentElement = elm.parentNode;
     this.canvas = document.createElement('canvas');
     elm.appendChild(this.canvas);
 
-    this.glContext = twgl.getWebGLContext(canvas);
+    this.glContext = twgl.getWebGLContext(this.canvas);
     this.programInfo = twgl.createProgramInfo(this.glContext , [vs, fs]);
 
     var arrays = {
@@ -22,20 +23,44 @@ VRtwglQuad = function() {
     this.bufferInfo = twgl.createBufferInfoFromArrays(this.glContext, arrays);
   }
 
-  this.resize = function(width, height) {
-    // dummy op, not w/h actually needed with twgl
-    twgl.resizeCanvasToDisplaySize(this.glContext.canvas);
+  this.resize = function() {
+    // Get the canvas from the WebGL context
+    var canvas = self.glContext.canvas;
+
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = self.parentElement.clientWidth;
+    var displayHeight = self.parentElement.clientHeight;
+
+    // Check if the canvas is not the same size.
+    if (canvas.width  != displayWidth ||
+        canvas.height != displayHeight) {
+
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+      canvas.height = displayHeight;
+
+      // Set the viewport to match
+      self.glContext.viewport(0, 0, canvas.width, canvas.height);
+    }
   }
 
   this.render = function() {
+    //console.log(self.glContext.canvas.width, self.glContext.canvas.height);
+    //console.log(self.canvas.clientWidth, self.canvas.clientHeight);
     var uniforms = {
-      resolution: [this.glContext.canvas.width, this.glContext.canvas.height],
+      resolution: [self.canvas.clientWidth, self.canvas.clientHeight],
     };
 
-    this.glContext.useProgram(this.programInfo.program);
-    twgl.setBuffersAndAttributes(this.glContext, this.programInfo, this.bufferInfo);
-    twgl.setUniforms(this.programInfo, uniforms);
-    twgl.drawBufferInfo(this.glContext, gl.TRIANGLES, this.bufferInfo);
+    self.glContext.useProgram(self.programInfo.program);
+    twgl.setBuffersAndAttributes(self.glContext, self.programInfo, self.bufferInfo);
+    twgl.setUniforms(self.programInfo, uniforms);
+    twgl.drawBufferInfo(self.glContext, self.glContext.TRIANGLES, self.bufferInfo);
+    // console.log("RENDER");
+  }
+
+  this.anim = function() {
+    self.render();
+    requestAnimationFrame(self.anim);
   }
 }
 
