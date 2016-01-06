eb23437c3ed0a5d1c312dd1344ab62fb6e745095
Bhautik Joshi <bjoshi+github@gmail.com>
2016-01-05 22:51:48 -0800

    compartmentalizing test

--- src/js/twglTest.js
+++ src/js/twglTest.js
@@ -1,13 +1,19 @@
-VRtwglQuadTest = require('./VRtwglQuadTest.js');
 
-console.log("INIT");
-var twglTestElements=document.getElementsByTagName("twglTest");
-for(twglTestIt = 0;twglTestIt < twglTestElements.length; twglTestIt++) {
-  console.log("FOUND");
-  var twglTestElement = twglTestElements[twglTestIt];
-  var vrTwglQuadTest = new VRtwglQuadTest();
-  vrTwglQuadTest.init(twglTestElement);
-  vrTwglQuadTest.resize();
-  window.addEventListener('resize', vrTwglQuadTest.resize, false);
-  vrTwglQuadTest.anim();
+// create a test quad from this element: <twglTest></twglTest>
+runTest = function() {
+  VRtwglQuadTest = require('./VRtwglQuadTest.js');
+
+  console.log("INIT");
+  var twglTestElements=document.getElementsByTagName("twglTest");
+  for(twglTestIt = 0;twglTestIt < twglTestElements.length; twglTestIt++) {
+    console.log("FOUND");
+    var twglTestElement = twglTestElements[twglTestIt];
+    var vrTwglQuadTest = new VRtwglQuadTest();
+    vrTwglQuadTest.init(twglTestElement);
+    vrTwglQuadTest.resize();
+    window.addEventListener('resize', vrTwglQuadTest.resize, false);
+    vrTwglQuadTest.anim();
+  }
 }
+
+runTest();
