8478944be2729aeb242e4b71a353c002feb2f562
Bhautik Joshi <bjoshi+github@gmail.com>
2016-01-31 20:45:55 -0800

    rewiring gyro pipeline

--- src/js/VRRotMath.js
+++ src/js/VRRotMath.js
@@ -1,5 +1,62 @@
 twgl = require('../js-ext/twgl-full.js');
 
+var matFromOrient = function( alpha, beta, gamma ) {
+  var d2r = Math.PI / 180;
+
+  var _x = beta  ? beta  * d2r : 0; // beta value
+  var _y = gamma ? gamma * d2r : 0; // gamma value
+  var _z = alpha ? alpha * d2r : 0; // alpha value
+
+  var cX = Math.cos( _x );
+  var cY = Math.cos( _y );
+  var cZ = Math.cos( _z );
+  var sX = Math.sin( _x );
+  var sY = Math.sin( _y );
+  var sZ = Math.sin( _z );
+
+  //
+  // ZXY rotation matrix construction.
+  //
+
+  var m11 = cZ * cY - sZ * sX * sY;
+  var m12 = - cX * sZ;
+  var m13 = cY * sZ * sX + cZ * sY;
+
+  var m21 = cY * sZ + cZ * sX * sY;
+  var m22 = cZ * cX;
+  var m23 = sZ * sY - cZ * cY * sX;
+
+  var m31 = - cX * sY;
+  var m32 = sX;
+  var m33 = cX * cY;
+
+  return [
+    m11,    m12,    m13, 0,
+    m21,    m22,    m23, 0,
+    m31,    m32,    m33, 0,
+    0,      0,      0,   1
+  ];
+}
+
+function matFromScreen( screenOrientation ) {
+  var d2r = Math.PI / 180;
+
+	var orientationAngle = screenOrientation ? screenOrientation * d2r : 0;
+
+	var cA = Math.cos( orientationAngle );
+	var sA = Math.sin( orientationAngle );
+
+	// Construct our screen transformation matrix
+	var r_s = [
+		cA,    -sA,    0,  0,
+		sA,    cA,     0,  0,
+		0,     0,      1,  0,
+    0,     0,      0,  1
+	];
+
+	return r_s;
+}
+
 // create quaternion from euler, YXZ order
 var setFromEulerYXZ = function (_x, _y, _z) {
   var c1 = Math.cos( _x / 2 );
@@ -113,12 +170,22 @@ var _matToEuler = function(mat) {
 }
 
 VRRotMath = function() {
-  // this.gyroToMat = function(_alpha, _beta, _gamma, _orientation) {
-  //
-  // }
   this.timer = 0.0;
-
   this.gyroToMat = function(_alpha, _beta, _gamma, _orientation) {
+    document.getElementById("log").innerHTML = Math.floor(_orientation);
+
+    var orientMat = matFromOrient(_alpha, _beta, _gamma);
+    var screenMat = matFromScreen(-_orientation);
+
+    var outMat = twgl.m4.multiply(orientMat, screenMat);
+
+    twgl.m4.rotateZ(outMat,-Math.PI/2,outMat);
+    twgl.m4.rotateX(outMat,Math.PI,outMat);
+
+    return outMat;
+  }
+
+  this._gyroToMat = function(_alpha, _beta, _gamma, _orientation) {
     // document.getElementById("log").innerHTML = "PING";
 
     // Rotation around the z-axis.
