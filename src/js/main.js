5de5ec8da7837dbd045d6f16ba60a05ad4146eda
Bhautik Joshi <bjoshi+github@gmail.com>
2016-02-03 20:43:58 -0800

    the google juice

--- src/js/main.js
+++ src/js/main.js
@@ -95,4 +95,17 @@ if (!window.VRURLPARSER_INIT){
 if (window.VRSTORYPARSER_INIT)
   return;
 
+
+//
+// some google juice - for inferring what screen sizes/resolutions
+// are using vrEmbed and what kind of legacy devices should be supported
+//
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-73312245-1', 'auto');
+ga('send', 'pageview');
+
 var storyParser = StoryParserFactory.getInstance();
