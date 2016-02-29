34bb1140a5d3afcca656dd814f3e2870dc74e014
Bhautik Joshi <bjoshi+github@gmail.com>
2016-02-28 23:23:36 -0800

    fix plane

--- src/js/VRCanvasFactory.js
+++ src/js/VRCanvasFactory.js
@@ -29,7 +29,6 @@ function VRCanvasBase() {
 
 VRCanvasBase.prototype.initBase = function(gl) {
   this.vrTextureDescription = new VRTextureDescription();
-  this.vrTextureDescription.plane = true;
   this.canvas = document.createElement("canvas");
   this.ctx = this.canvas.getContext("2d");
   this.ctx.canvas.width  = 2048;
