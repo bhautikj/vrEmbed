3b40b272c37eee97a08e6b2bdffdd28a4079cf04
Bhautik Joshi <bjoshi+github@gmail.com>
2016-03-06 22:43:55 -0800

    WIP

--- src/js/VRCreateUI.js
+++ src/js/VRCreateUI.js
@@ -1,65 +1,5 @@
 var VRScene = require('./VRScene.js');
 
-// via: http://stackoverflow.com/questions/22575636/how-to-check-if-a-canvas-element-has-been-tainted/22580129#22580129
-var newImage = function(src,callWhenTainted,callWhenNotTainted){
-  // tmpCanvas to test CORS
-  var tmpCanvas=document.createElement("canvas");
-  var tmpCtx=tmpCanvas.getContext("2d");
-  // tmpCanvas just tests CORS, so make it 1x1px
-  tmpCanvas.width=tmpCanvas.height=1;
-
-  var img=new Image();
-  var isLoaded =false;
-  // set the cross origin flag (and cross our fingers!)
-  img.crossOrigin="anonymous";
-  img.onload=function(){
-    if (isLoaded)
-      return;
-    // add a tainted property to the image
-    // (initialize it to true--is tainted)
-    img.tainted=true;
-    // draw the img on the temp canvas
-    tmpCtx.drawImage(img,0,0);
-    // just in case this onload stops on a CORS error...
-    // set a timer to call afterOnLoad shortly
-    setTimeout(function(){
-       afterOnLoad(img,callWhenTainted,callWhenNotTainted);
-    },500);  // you can probably use less than 1000ms
-    // try to violate CORS
-    var i=tmpCtx.getImageData(1,1,1,1);
-    // if we get here, CORS is OK so set tainted=false
-    img.tainted=false;
-    isLoaded =true;
-  };
-
-  if(img.addEventListener) {
-    img.addEventListener('error', function (e) {
-        e.preventDefault(); // Prevent error from getting thrown
-        // Handle error here
-        callWhenTainted(img);
-    });
-  }
-  img.src=src;
-  return(img);
-}
-
-// called from image.onload
-// at this point the img.tainted flag is correct
-var afterOnLoad = function(img,callWhenTainted,callWhenOK){
-  if(img.tainted){
-    // it's tainted
-    callWhenTainted(img);
-  } else {
-    // it's OK, do dataURL stuff
-    callWhenOK(img);
-  }
-}
-
-var imageNotOK = function(img) {
-  var msg = "Cannot load image: " + img.src + " - problem loading image or host does not allow remote loading of images (CORS error)."
-  console.log(msg);
-}
-
 var NumberSlider = function() {
   var self=this;
   this.sliderElement = null;
@@ -109,6 +49,7 @@ var NumberSlider = function() {
 
 VRCreateUI = function() {
   var self=this;
+  this.firstRun = true;
   this.storyManager = null;
   this.hfovNumberSlider = null;
   this.vfovNumberSlider = null;
@@ -117,6 +58,7 @@ VRCreateUI = function() {
 
   this.photoIdx = 0;
   this.sceneIdx = 0;
+  this.sceneList = [];
 
 
   this.getStory = function() {
@@ -135,12 +77,26 @@ VRCreateUI = function() {
     self.dict.photoObjects[self.photoIdx].textureDescription.sphereFOV = [hfov,vfov];
     self.dict.photoObjects[self.photoIdx].textureDescription.sphereCentre = [xpos,ypos];
 
-    self.vrScene.initDict(self.dict);
+    self.getStory().sceneList[self.sceneIdx].initDict(self.dict);
     self.getStory().setupScene(self.sceneIdx);
   }
 
-  this.proceed = function(img) {
-    self.vrScene = new VRScene();
+  this.firstTime = function() {
+    if (self.firstRun==false)
+      return;
+
+    var vrScene = new VRScene();
+    self.getStory().sceneList.push(vrScene);
+
+    self.firstRun = false;
+  }
+
+  this.loadImage = function() {
+    self.firstTime();
+
+    var imageURL = document.getElementById('imageURL').value;
+    document.getElementById('imageSettingsDiv').hidden=false;
+
     self.dict = {};
     self.dict.photoObjects=[];
     self.dict.textObjects=[];
@@ -155,19 +111,12 @@ VRCreateUI = function() {
     photo.textureDescription.sphereCentre = [0,0];
     self.dict.photoObjects.push(photo);
 
-
     // console.log(dict);
-    self.vrScene.initDict(self.dict);
-
-    self.getStory().sceneList.push(self.vrScene);
-    self.getStory().currentSceneIndex = 0;
-    self.getStory().setupScene(0);
+    self.getStory().sceneList[self.sceneIdx].initDict(self.dict);
+    self.getStory().currentSceneIndex = self.sceneIdx;
+    self.getStory().setupScene(self.sceneIdx);
   }
 
-  this.loadImage = function() {
-    var imageURL = document.getElementById('imageURL').value;
-    var img = newImage(imageURL, imageNotOK, self.proceed);
-  }
 
   this.init = function(vrStoryManager) {
     document.getElementById('imageURL').value = "../src/assets/rheingauer_dom.jpg";
@@ -200,6 +149,7 @@ VRCreateUI = function() {
                               0,
                               this.sliderChange);
 
+    document.getElementById('imageURL').onchange = this.loadImage;
    }
 }
 
