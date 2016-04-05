49f3d02baa4168e04894a2b9ac13de8f8ce730b7
Bhautik Joshi <bjoshi+github@gmail.com>
2016-04-05 00:51:19 -0700

    moved url parsing back to index

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,3 +1,19 @@
-var VRImageLoader = require('./VRImageLoader.js');
+var vrImageLoader = require('./VRImageLoader.js');
+
+// var vrImageLoader = new VRImageLoader();
+
+vrImageLoader.init(vrEmbed);
+
+if(vrImageLoader.hasURLParams()) {
+  if (vrImageLoader.isGalleryURL()) {
+    vrEmbed.clearPage();
+    vrEmbed.initFullpage();
+    vrImageLoader.getImages(vrImageLoader.getURLParameters()["gallery"]);
+  } else if (vrImageLoader.isImageURL()) {
+    vrEmbed.clearPage();
+    vrEmbed.initFullpage();
+    vrImageLoader.buildFromImageURLParams(vrImageLoader.getURLParameters());
+  }
+}
 
 module.exports = VRImageLoader;
